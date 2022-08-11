56bb2b0d02028ad52316e30fd574a2e5096a65b7
Neerja Katiyar <neerjabiotechinfo@gmail.com>
2022-08-11 14:38:19 -0400

    Added script for UMAP and featureplots from Tabula Muris single cell data.

---
+++ script_UMAP_featureplot.R
@@ -0,0 +1,27 @@
+
+library("Seurat")
+library("BiocFileCache")
+library(ggplot2)
+library(ggpubr)
+library(paletteer)
+
+palette <- RColorBrewer::brewer.pal(9, "Reds")
+palette[1] <- "#adadad"
+
+#Visualize data with Nebulosa.
+tm_obj <- readRDS("../spleen_seurat_subset_rename_ident_Oct24.rds")
+summary(tm_obj@meta.data)
+
+#tm_obj <- RunUMAP(tm_obj, dims = 1:10)
+#pdf("UMAP_Tabula_muris.pdf")
+#DimPlot(tm_obj, reduction = "umap")
+#dev.off()
+
+pdf("UMAP_Tabula_muris_split_age.pdf")
+DimPlot(tm_obj, reduction = "umap", split.by = "age")
+dev.off()
+
+pdf("spleen_Jun_Fos_Fosb.pdf", width = 9, height = 10)
+FeaturePlot(tm_obj, features = c("Jun", "Fos", "Fosb"), cols = palette, pt.size = 1, order = TRUE,  split.by = "age")
+dev.off()
+
